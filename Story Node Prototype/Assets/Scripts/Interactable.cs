0ebbc8bc62e9166487894636b2c9262d4eedbfd7
Benjy96 <benjamindickey@hotmail.co.uk>
2017-10-18 23:15:41 +0100

    Add TODO: Investigate External Function Binding

--- Story Node Prototype/Assets/Scripts/Interactable.cs
+++ Story Node Prototype/Assets/Scripts/Interactable.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public abstract class InteractableNPC : MonoBehaviour {
 
     protected static PlayerController player;
-    protected static bool externalsSet = false;
+    protected static bool externalsSet = false; 
+    //TODO: Find a different way of setting externals - potential problem: If we had two classes that inherit from interactableNPC, only one type's external will be set
+    //TODO: Investigate externals - I foresee two different ways: Set externals in the main Script script, or be able to check if an external function is set:
+                                                                                                          //This will be either a property of Story, or a bool for each subclass
 
     public Script story;
     public string inkPath = "";
