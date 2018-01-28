4b21aa71fafbf6412061c8ee40cfef0bbcceb989
Benjy96 <benjamindickey@hotmail.co.uk>
2018-01-28 19:12:27 +0000

    Add singleton game manager

---
+++ Story Node Prototype/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //Singleton & accessor
+    private static GameManager _GameManager = null;
+    public static GameManager Instance { get { return _GameManager; } }
+
+    private void Awake()
+    {
+        //Singleton setup
+        if (_GameManager == null)
+        {
+            _GameManager = this;
+        }
+        else if (_GameManager != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
