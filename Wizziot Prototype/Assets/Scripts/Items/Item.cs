f9b226492fbc102ebadafbb279ba6c2068fe5f43
Benjy96 <benjamindickey@hotmail.co.uk>
2018-02-06 15:44:32 +0000

    Carry object when in inventory

--- Wizziot Prototype/Assets/Scripts/Items/Item.cs
+++ Wizziot Prototype/Assets/Scripts/Items/Item.cs
@@ -2,8 +2,13 @@
 
 public class Item : Targetable {
 
+    private Transform player;
+
     public float interactionRadius = 5f;
     public ItemAttributes itemAttributes;
+
+    new public string name = "New item";
+    public Sprite icon = null;
 
     private void Start()
     {
@@ -12,15 +17,22 @@ public class Item : Targetable {
 
     public void PickUp(Transform player)
     {
+        this.player = player;
+
         if((player.transform.position - transform.position).sqrMagnitude < interactionRadius)
         {
             Inventory.Instance.Add(this);
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+            gameObject.transform.SetParent(player);
+            //Destroy(gameObject); //use this if going to use ScriptableObjects <- destroy the ITEM, store the asset
         }
     }
 
     public virtual void Use()
     {
         Debug.Log("Attempting to use " + itemAttributes.name);
+        gameObject.transform.position = player.position;
+        gameObject.transform.SetParent(null);
+        gameObject.SetActive(true);
     }
 }
