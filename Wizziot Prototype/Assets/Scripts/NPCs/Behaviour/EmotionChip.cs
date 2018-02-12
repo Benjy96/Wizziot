657989d484e638a3336dea69f8ad00208da96d72
Benjy96 <benjamindickey@hotmail.co.uk>
2018-02-12 21:52:38 +0000

    Refactor EmotionChip - Comments, Summaries, and Separate Implementation into Region

--- Wizziot Prototype/Assets/Scripts/NPCs/Behaviour/EmotionChip.cs
+++ Wizziot Prototype/Assets/Scripts/NPCs/Behaviour/EmotionChip.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 /// A variant of a finite state machine... The EMOTIONAL State Machine (ESM)
 /// Each agent with an emotion chip has an emotional disposition. 
 /// External factors can influence this agent's emotions; the way in which each emotion is affected differs between emotional dispositions. 
-/// The extent to which each emotion differs is then affected by weight values.
+/// The extent to which each emotion is influenced is then affected by weight values.
+/// 
+/// TL;DR - This class is the logic that determines what behaviour an AI undertakes. If it's angry, then the "angry state" will execute.
 /// </summary>
 public class EmotionChip : MonoBehaviour {
 
@@ -29,6 +31,76 @@ public class EmotionChip : MonoBehaviour {
     public State angryGoal;
     public State scaredGoal;
 
+    /// <summary>
+    /// This method provides a way in which to influence this agent's emotional state
+    /// </summary>
+    /// <param name="intent">The way in which the actor intends to influence this agent</param>
+    /// <param name="amount">How much to influence the agent's emotions in the range of 0 to 1</param>
+    public void Influence(Emotion intent, float amount)
+    {
+        amount = Mathf.Clamp(amount, 0f, 1f);
+
+        //Influence the NPC by the external factor's intent differently, based upon this agent's emotional disposition
+        switch (disposition)
+        {
+            //If agent is predisposed to calm, handle the intended emotional influences as such:
+            case Emotion.Calm:
+                switch (intent)
+                {
+                    case Emotion.Calm:  //If actor intends to calm the agent, do it scaled by a factor of the agent's trust
+                        agentEmotions[intent] += amount * trust;
+                        break;
+
+                    case Emotion.Anger: //If actor intends to scare or anger enemy, anger/scare them, but reduced by a factor of their trust (harder to anger/scare if calm)
+                    case Emotion.Fear:
+                        foreach (KeyValuePair<Emotion, float> agentEmotion in agentEmotions)
+                        {
+                            if (agentEmotion.Key != disposition) agentEmotions[agentEmotion.Key] += amount / trust;
+                        }
+                        break;
+                }
+                break;
+
+            //If agent is predisposed to anger:
+            case Emotion.Anger:
+                switch (intent)
+                {
+                    case Emotion.Calm:  //If actor intends to calm the enemy, calm them, but reduce by a factor of their irascibility
+                        agentEmotions[intent] += amount / irascibility;
+                        break;
+
+                    case Emotion.Anger: //If actor intends to anger the enemy, increase the intent by a factor of their irascibility
+                        agentEmotions[intent] += amount * irascibility;
+                        break;
+
+                    case Emotion.Fear:  //If actor intends to scare enemy, simply anger the agent
+                        agentEmotions[Emotion.Anger] += amount;
+                        break;
+                }
+                break;
+
+            //If agent is predisposed to fear:
+            case Emotion.Fear:
+                switch (intent)
+                {
+                    case Emotion.Calm:  //If actor intends to calm the agent, calm it, but reduce calming effect in relation to the agent's cowardice
+                        agentEmotions[intent] += amount / cowardice;
+                        break;
+
+                    case Emotion.Anger: //If actor intends to anger the enemy, anger them, but anger them slowly in relation to cowardice
+                        agentEmotions[intent] += amount / cowardice;
+                        break;
+
+                    case Emotion.Fear:  //If actor intends to scare enemy, scare them by a factor of the intent and cowardice
+                        agentEmotions[intent] += amount * cowardice;
+                        break;
+                }
+                break;
+        }
+        ScaleEmotions();    //This reduces the complexity of each assignment to emotion - we can go over 1 all the time without issue.
+    }
+
+    #region Implementation
     private void Awake()
     {
         //Store each emotion type in a state variable
@@ -64,104 +136,38 @@ public class EmotionChip : MonoBehaviour {
         }
 
         //Step 2. Tend towards disposition
-        foreach (KeyValuePair<Emotion, float> agentEmotion in agentEmotions)
+        foreach (KeyValuePair<Emotion, float> agentEmotionPair in agentEmotions)
         {
             //If disposition, tend towards max, else tend towards minimum
-            if (agentEmotion.Key == disposition)
+            if (agentEmotionPair.Key == disposition)
             {
-                agentEmotions[agentEmotion.Key] = Mathf.Lerp(agentEmotions[disposition], 1f, Time.deltaTime);
+                agentEmotions[agentEmotionPair.Key] = Mathf.Lerp(agentEmotions[disposition], 1f, Time.deltaTime);
             }
             else
             {
-                agentEmotions[agentEmotion.Key] = Mathf.Lerp(agentEmotions[agentEmotion.Key], 0f, Time.deltaTime);
+                agentEmotions[agentEmotionPair.Key] = Mathf.Lerp(agentEmotions[agentEmotionPair.Key], 0f, Time.deltaTime);
             }
         }
     }
 
-    /// <summary>
-    /// This method provides a way in which to influence this agent's emotional state
-    /// </summary>
-    /// <param name="intent">The way in which the actor intends to influence this agent</param>
-    /// <param name="amount">How much to influence the agent's emotions in the range of 0 to 1</param>
-    public void Influence(Emotion intent, float amount)
-    {
-        amount = Mathf.Clamp(amount, 0f, 1f);
-        
-        //Influence the NPC by the external factor's intent differently, based upon this agent's emotional disposition
-        switch (disposition)
-        {
-            //If agent is predisposed to calm, handle the intended emotional influences as such:
-            case Emotion.Calm:
-                switch (intent)
-                {
-                    case Emotion.Calm:  //If actor intends to calm the agent, do it scaled by a factor of the agent's trust
-                        agentEmotions[intent] += amount * trust;    
-                        break;
-
-                    case Emotion.Anger: //If actor intends to scare or anger enemy, anger/scare them, but reduced by a factor of their trust (harder to anger/scare if calm)
-                    case Emotion.Fear:
-                        foreach (KeyValuePair<Emotion, float> agentEmotion in agentEmotions)
-                        {
-                            if (agentEmotion.Key != disposition) agentEmotions[agentEmotion.Key] += amount / trust;
-                        }
-                            break;
-                }
-                break;
-            
-            //If agent is predisposed to anger:
-            case Emotion.Anger:
-                switch (intent)
-                {
-                    case Emotion.Calm:  //If actor intends to calm the enemy, calm them, but reduce by a factor of their irascibility
-                        agentEmotions[intent] += amount / irascibility; 
-                        break;
-
-                    case Emotion.Anger: //If actor intends to anger the enemy, increase the intent by a factor of their irascibility
-                        agentEmotions[intent] += amount * irascibility; 
-                        break;
-
-                    case Emotion.Fear:  //If actor intends to scare enemy, simply anger the agent
-                        agentEmotions[Emotion.Anger] += amount; 
-                        break;
-                }
-                break;
-
-            //If agent is predisposed to fear:
-            case Emotion.Fear:
-                switch (intent)
-                {
-                    case Emotion.Calm:  //If actor intends to calm the enemy, calm them, but reduce it in relation to the agent's cowardice
-                        agentEmotions[intent] += amount / cowardice;    
-                        break;
-
-                    case Emotion.Anger: //If actor intends to anger the enemy, anger them, but anger them slowly in relation to cowardice
-                        agentEmotions[intent] += amount / cowardice;    
-                        break;
-
-                    case Emotion.Fear:  //If actor intends to scare enemy, scare them by a factor of the intent and cowardice
-                        agentEmotions[intent] += amount * cowardice;    
-                        break;
-                }
-                break;
-        }
-        ScaleEmotions();    //This reduces the complexity of each assignment to emotion - we can go over 1 all the time without issue.
-    }
+    
 
     //Scale the emotions to be within the range [0:1]
     private void ScaleEmotions()
     {
         float totalValue = 0f;
 
-        foreach (KeyValuePair<Emotion, float> enemyEmotion in agentEmotions)
+        foreach (KeyValuePair<Emotion, float> agentEmotionPair in agentEmotions)
         {
-            totalValue += enemyEmotion.Value;
+            totalValue += agentEmotionPair.Value;
         }
 
-        foreach (KeyValuePair<Emotion, float> enemyEmotion in agentEmotions)
+        foreach (KeyValuePair<Emotion, float> agentEmotionPair in agentEmotions)
         {
-            agentEmotions[enemyEmotion.Key] = agentEmotions[enemyEmotion.Key] / totalValue;
+            agentEmotions[agentEmotionPair.Key] = agentEmotions[agentEmotionPair.Key] / totalValue;
         }
     }
+#endregion
 }
 
 public enum Emotion { Calm, Anger, Fear }
