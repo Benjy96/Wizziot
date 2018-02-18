55f6fae61c832d66d7ed3e313581d9ecf65e07a9
Benjy96 <benjamindickey@hotmail.co.uk>
2018-02-18 14:54:02 +0000

    Add AudioManager and Sound blueprint class
    Any script can access the Instance of the Audio Manager and play a sound that has been added to the "Sounds" array held by the AudioManager and added to by the designers.

---
+++ Wizziot Prototype/Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    private static AudioManager _Audio_Manager;
+    public static AudioManager Instance { get { return _Audio_Manager; } }
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        //Singleton
+        if(_Audio_Manager == null)
+        {
+            _Audio_Manager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach (Sound sound in sounds)
+        {
+            //Add AudioSource to AudioManager from list of Sounds
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.soundClip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    private void Start()
+    {
+        //Play main things... e.g. "Theme"
+    }
+
+    public void Play(string soundName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == soundName);
+        if(s != null)
+        {
+            s.source.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioManager: " + soundName + " not found!");
+        }
+    }
+}
---
+++ Wizziot Prototype/Assets/Scripts/Game/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+    //Name and clip
+    public string name;
+    public AudioClip soundClip;
+
+    //Configuration of sound
+    [Range(0f, 1f)] public float volume;
+    [Range(0.1f, 3f)] public float pitch;
+    public bool loop;
+
+    //GameObject component that houses the above sound "data"
+    [HideInInspector] public AudioSource source;
+}
