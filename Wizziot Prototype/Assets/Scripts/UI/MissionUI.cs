e57f3d4ac55284f9b6ab1eb250749ae04948571e
Benjy96 <benjamindickey@hotmail.co.uk>
2018-03-04 21:16:17 +0000

    Add description to Compass rotation - trig & unit circle & vectors, etc

--- Wizziot Prototype/Assets/Scripts/UI/MissionUI.cs
+++ Wizziot Prototype/Assets/Scripts/UI/MissionUI.cs
@@ -16,29 +16,35 @@ public class MissionUI : MonoBehaviour {
         currentWaypoint = mission.location;
     }
 
+    //Rotate compass - rotates differently depending on Camera State (Look or Follow) - a bunch of trigonometry, so beware
     private void Update()
     {
         if(currentWaypoint != null)
         {
-            Vector3 camPos = Camera.main.transform.position;
             Transform player = PlayerManager.Instance.player.GetComponent<Transform>();
 
             Vector3 playerPos = player.position;
             Vector3 direction = (currentWaypoint - playerPos).normalized;
 
-            //Unit circle trigonometry: https://www.khanacademy.org/math/algebra2/trig-functions/unit-circle-definition-of-trig-functions-alg2/a/trig-unit-circle-review
-            //Get point on 2D unit circle
+            //Unit circle & trigonometry: https://www.khanacademy.org/math/algebra2/trig-functions/unit-circle-definition-of-trig-functions-alg2/a/trig-unit-circle-review
+            /** Get point on 2D unit circle:
+             * Think of the 3D world from above as a graph
+             * Direction vector made up of x and z (x and y of a 2d graph)
+             * We then use the x and y to calculate the hypoteneuse
+             * Using the hypoteneuse we then get the angle between the player and waypoint using (inverse) trigonometry (SOH CAH TOA)
+             * Then use this angle to rotate the compass
+             * */
             float hypoteneuse = Mathf.Sqrt((direction.x * direction.x) + (direction.z * direction.z));  //Used for trig (calculating angle - Cos = A/H)
             float angle = Mathf.Acos(direction.z / hypoteneuse) * Mathf.Rad2Deg;    //Angle to waypoint
 
             Quaternion q = new Quaternion();
 
-            //Use this for rotating the compass DEPENDING on the player's rotation, rather than the world's
-            float camAngle = player.rotation.eulerAngles.y;
-
             //Correct rotation (trig are repeating functions - need to flip the compass when on "other side" of waypoint)
             if (PlayerCamera.State == PlayerCamera.CameraMode.Follow)
             {
+                //Use this for rotating the compass DEPENDING on the player's rotation, rather than the world's
+                float camAngle = player.rotation.eulerAngles.y;
+
                 if (direction.x < 0)
                 {
                     q = Quaternion.AngleAxis(angle + camAngle, Vector3.forward);
