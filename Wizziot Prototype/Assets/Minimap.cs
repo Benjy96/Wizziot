9981a063cb40b023136d619a40db8943cbe1517d
Benjy96 <benjamindickey@hotmail.co.uk>
2018-03-23 16:56:59 +0000

    Minmap

---
+++ Wizziot Prototype/Assets/Minimap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = PlayerManager.Instance.player.transform.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+}
