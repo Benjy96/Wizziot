6cfa19735024ecc29856996f40cfc3dc1855eb4c
Benjy96 <benjamindickey@hotmail.co.uk>
2018-03-29 16:25:53 +0100

    Add Movement Behaviour Scripts: Hover & Look Around

---
+++ Wizziot Prototype/Assets/Scripts/NPCs/Movement/Hover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    float startY;
+    [Range(0f, 1f)] public float hoverRange = 1f;
+
+    private void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        //Speed is time, range is hoverRange
+        transform.position = new Vector3(transform.position.x, startY + Mathf.Sin(Time.time) * hoverRange, transform.position.z);
+    }
+}
---
+++ Wizziot Prototype/Assets/Scripts/NPCs/Movement/LookAround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour {
+
+    public float angleToLookEachSide = 15f;
+    [Range(0f, 1f)] public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.rotation.y > angleToLookEachSide)
+        {
+            transform.Rotate(Vector3.up, Mathf.Sin(Time.time) * speed);
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, Mathf.Cos(Time.time) * speed);
+        }
+    }
+}
