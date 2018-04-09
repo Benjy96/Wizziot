f1c8517359eb51fc72220c4e9dc8a82371404cad
Benjy96 <benjamindickey@hotmail.co.uk>
2018-04-09 09:44:43 +0100

    Revert "Add Fixed/Unfixed Transit Portal Prefabs and Second Platform"
    This reverts commit e5c540d29782f43dcfe0a47bc48a143a82e8e4c1.

--- Wizziot Prototype/Assets/Scripts/NPCs/Enemies/NPCAim.cs
+++ Wizziot Prototype/Assets/Scripts/NPCs/Enemies/NPCAim.cs
@@ -9,6 +9,5 @@ public class NPCAim : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = PlayerManager.Instance.player.GetComponent<Transform>();
-        transform.position = player.position;
 	}
 }
