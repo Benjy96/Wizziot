98b5f9ac0deffcefcbcd69e92d7aa0ace7781a19
Benjy96 <benjamindickey@hotmail.co.uk>
2018-05-02 22:41:00 +0100

    Reduce sight range of area abilities scaled by game difficulty

--- Wizziot Prototype/Assets/Scripts/Player/Aim.cs
+++ Wizziot Prototype/Assets/Scripts/Player/Aim.cs
@@ -2,11 +2,21 @@
 
 public class Aim : MonoBehaviour {
 
+    private float range;
+    private bool gotRangeVal = false;
+
     void Update () {
+        if (gotRangeVal == false)
+        {
+            range = Mathf.Sqrt(PlayerManager.Instance.playerStats.sqrMaxTargetDistance);
+            range /= GameMetaInfo._DIFFICULTY_SCALE;
+            gotRangeVal = true;
+        }
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit pointHit;
 
-        if (Physics.Raycast(ray, out pointHit, 100f, LayerMask.GetMask("Ground")))
+        if (Physics.Raycast(ray, out pointHit, range, LayerMask.GetMask("Ground")))
         {
             transform.position = pointHit.point;
         }
